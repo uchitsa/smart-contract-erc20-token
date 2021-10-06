21b2087e8aeec47c461283981e33d205527f9efb
uchitsa <uchitsa@ya.ru>
2021-10-07 02:41:25 +0300

    Create TTToken.sol

---
+++ contracts/TTToken.sol
@@ -0,0 +1,12 @@
+
+// contracts/TTToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TTToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TTT", "TTT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
